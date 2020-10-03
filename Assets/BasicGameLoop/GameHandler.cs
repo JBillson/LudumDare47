f7ff34cca2a8b04d1ec94972b787d03cf4a70ebc
Daniel Rousseau <danrousseau@protonmail.com>
2020-10-03 15:56:57 +0200

    Game now restars when time runs out

--- Assets/BasicGameLoop/GameHandler.cs
+++ Assets/BasicGameLoop/GameHandler.cs
@@ -2,39 +2,62 @@
 using BasicGameLoop;
 using FpsController;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 
 public class GameHandler : MonoBehaviour
 {
     public Action GameWon;
+    public Action GameLoss;
 
     private Transform winPoint;
     private Transform playerPos;
+
+    [SerializeField] private GameObject WinCanvas;
+    [SerializeField] private GameObject LoseCanvas;
+
+    [SerializeField] private float DungeonTime;
 
     private void Start()
     {
         playerPos = FindObjectOfType<PlayerController>().transform;
         winPoint = FindObjectOfType<WinPoint>().transform;
-        GameWon += GameOver;
+        GameWon += DungeonComplete;
+        GameLoss += GameOver;
     }
+
 
     private void Update()
     {
+        DungeonTime -= Time.deltaTime;
+        
         if (CheckDistToPlayer())
         {
             GameWon?.Invoke();
+        }
+
+        if (DungeonTime <= 0)
+        {
+            GameLoss?.Invoke();
         }
     }
 
     private bool CheckDistToPlayer()
     {
         var dist = Vector3.Distance(winPoint.position, playerPos.position);
-
-        return dist <= 1;
+        return dist <= 3;
     }
 
+    private void DungeonComplete()
+    {
+        WinCanvas.SetActive(true);
+        Debug.Log("Dungeon Complete");
+    }
+    
     private void GameOver()
     {
-        Debug.Log("Game Over");
+        var currentScene = SceneManager.GetActiveScene();
+
+        SceneManager.LoadScene(currentScene.name);
     }
 }
