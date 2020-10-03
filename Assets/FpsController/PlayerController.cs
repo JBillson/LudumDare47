003dd23d79753a76b7b3ebb7c33f6a4ace044887
Daniel Rousseau <danrousseau@protonmail.com>
2020-10-03 13:03:23 +0200

    Added FPS controller with double jump, still need to add dash

---
+++ Assets/FpsController/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace FpsController
+{
+    public class PlayerController : SerializedMonoBehaviour
+    {
+        private const float Gravity = -9.81f;
+
+        [SerializeField] private float MovementSpeed = 12;
+        [SerializeField] private float JumpHeight    = 3f;
+
+        [SerializeField, Range(1, 2)] private float InAirSpeedModifier;
+
+        private CharacterController CharacterController { get; set; }
+        private Vector3             _velocity;
+
+        private int _jumpCount;
+        private int maxJumpCount = 2;
+
+        private void Awake()
+        {
+            CharacterController = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            GroundSanityCheck();
+
+            var x = Input.GetAxis("Horizontal");
+            var z = Input.GetAxis("Vertical");
+
+            if (Input.GetKeyDown(KeyCode.Space) && _jumpCount < maxJumpCount)
+            {
+                _velocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+                _jumpCount++;
+            }
+
+            _velocity.y += Gravity * Time.deltaTime;
+
+            var playerTransform = transform;
+            var moveVector = playerTransform.right * x + playerTransform.forward * z;
+
+            MoveController(moveVector);
+        }
+
+        private void MoveController(Vector3 moveVector)
+        {
+            if (!CharacterController.isGrounded)
+                CharacterController.Move(moveVector * (MovementSpeed / InAirSpeedModifier * Time.deltaTime));
+            else
+                CharacterController.Move(moveVector * (MovementSpeed * Time.deltaTime));
+
+            CharacterController.Move(_velocity * Time.deltaTime);
+        }
+
+        private void GroundSanityCheck()
+        {
+            if (CharacterController.isGrounded && _velocity.y < 0)
+            {
+                _jumpCount = 0;
+                _velocity.y = -3f;
+            }
+        }
+    }
+}
---
+++ Assets/FpsController/PlayerLook.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace FpsController
+{
+    public class PlayerLook : MonoBehaviour
+    {
+        [SerializeField] private float mouseSensitivity = 150f;
+
+        private Transform _playerBody;
+        private float     _currentXRotation = 0f;
+
+        private void Awake()
+        {
+            _playerBody = FindObjectOfType<PlayerController>().gameObject.transform;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            GetLookVector();
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void GetLookVector()
+        {
+            var xRot = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            var yRot = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            RotateGameObject(yRot, xRot, _playerBody);
+        }
+
+        private void RotateGameObject(float yRot, float xRot, Transform toTurn)
+        {
+            _currentXRotation -= yRot;
+            _currentXRotation = Mathf.Clamp(_currentXRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(_currentXRotation, 0f, 0f);
+            toTurn.Rotate(Vector3.up * xRot);
+        }
+    }}
